6131232ed2a32afc384c219bccddc3d40b4a497a
unknown <derek.slauson5@gmail.com>
2023-08-20 21:11:24 -0500

    Updating login to check pw hash.

--- src/main/java/com/pairlearning/expensetracker/repositories/UserRepositoryImpl.java
+++ src/main/java/com/pairlearning/expensetracker/repositories/UserRepositoryImpl.java
@@ -49,7 +49,7 @@ public class UserRepositoryImpl implements UserRepository {
     public User findByEmailAndPassword(String email, String password) throws EtAuthException {
         try {
             User user = jdbcTemplate.queryForObject(SQL_FIND_BY_EMAIL, new Object[]{email}, userRowMapper);
-            if(!password.equals(user.getPassword()))
+            if(!BCrypt.checkpw(password, user.getPassword()))
                 throw new EtAuthException("Invalid email and/or password.");
             return user;
         } catch(EmptyResultDataAccessException e)  {
