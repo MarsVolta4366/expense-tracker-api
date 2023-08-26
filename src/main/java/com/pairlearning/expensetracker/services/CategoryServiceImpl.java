f87a6871600d98d6f7f30c8d0d3fdb76d2fb2928
unknown <derek.slauson5@gmail.com>
2023-08-26 17:41:26 -0500

    Added get all categories endpoint.

--- src/main/java/com/pairlearning/expensetracker/services/CategoryServiceImpl.java
+++ src/main/java/com/pairlearning/expensetracker/services/CategoryServiceImpl.java
@@ -19,7 +19,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> fetchAllCategories(Integer userId) {
-        return null;
+        return categoryRepository.findAll(userId);
     }
 
     @Override
