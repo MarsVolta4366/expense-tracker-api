c8df15b8d7559de1c1bd89222a19d3d18eaafdf6
unknown <derek.slauson5@gmail.com>
2023-08-27 09:32:45 -0500

    Added update category endpoint.

--- src/main/java/com/pairlearning/expensetracker/repositories/CategoryRepositoryImpl.java
+++ src/main/java/com/pairlearning/expensetracker/repositories/CategoryRepositoryImpl.java
@@ -26,6 +26,8 @@ public class CategoryRepositoryImpl implements CategoryRepository {
             "FROM ET_TRANSACTIONS T RIGHT OUTER JOIN ET_CATEGORIES C ON C.CATEGORY_ID = T.CATEGORY_ID " +
             "WHERE C.USER_ID = ? AND C.CATEGORY_ID = ? GROUP BY C.CATEGORY_ID";
     private static final String SQL_CREATE = "INSERT INTO ET_CATEGORIES (CATEGORY_ID, USER_ID, TITLE, DESCRIPTION) VALUES(NEXTVAL('ET_CATEGORIES_SEQ'), ?, ?, ?)";
+    private static final String SQL_UPDATE = "UPDATE ET_CATEGORIES SET TITLE = ?, DESCRIPTION = ? " +
+            "WHERE USER_ID = ? AND CATEGORY_ID = ?";
 
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -63,7 +65,11 @@ public class CategoryRepositoryImpl implements CategoryRepository {
 
     @Override
     public void update(Integer userId, Integer categoryId, Category category) throws EtBadRequestException {
-
+        try {
+            jdbcTemplate.update(SQL_UPDATE, new Object[]{category.getTitle(), category.getDescription(), userId, categoryId});
+        } catch(Exception e) {
+            throw new EtBadRequestException("Invalid request.");
+        }
     }
 
     @Override
